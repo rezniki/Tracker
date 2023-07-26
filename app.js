475b23c98917701bc0b4449918c345d59c87b350
rezniki <igorreznik2001@gmail.com>
2023-07-26 18:24:23 +0300

    Created new block: tracker__item

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 let trackerText = document.querySelector('.tracker__text');
 let trackerNumber = document.querySelector('.tracker__amount__number');
 let trackerButton = document.querySelector('.tracker__button');
+let trackerItem = document.querySelector('.tracker__item');
+let trackerItemText = document.querySelector('.tracker__item__text');
+let trackerItemBalance = document.querySelector('.tracker__item__balance');
+let trackerBalanceNumber = document.querySelector('.tracker__balance__number'); 
 
 trackerButton.onclick = function() {
-    let newTrackerCardIncome = document.createElement('div');
-    newTrackerCardIncome.className = 'tracker__card__income';
-    let newTrackerIncome = document.createElement('p');
-    newTrackerIncome.className = 'tracker__income';
-    let trackerButtonText = trackerText.value;
-    let trackerButtonNumber = trackerNumber.value;
+    let newTrackerItem = document.createElement('div');
+    newTrackerItem.className = 'tracker__item';
+    let newTrackerItemText = document.createElement('p');
+    newTrackerItemText.className = 'tracker__item__text';
+    let newTrackerItemBalance = document.createElement('p');
+    newTrackerItemBalance.className = 'tracker__item__balance';
+    trackerItemText = trackerText.value;
+    trackerItemBalance = trackerNumber.value;
+    newTrackerItemText.textContent = `${trackerItemText}`;
+    newTrackerItemBalance.textContent = `${trackerItemBalance}`;
+    newTrackerItem.append(newTrackerItemText, newTrackerItemBalance);
+    trackerItem.append(newTrackerItem);
 }
