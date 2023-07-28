b29404c37427b11a037647ad1a206904d0198a33
rezniki <igorreznik2001@gmail.com>
2023-07-28 23:42:11 +0300

    Created objects for save data

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
-let trackerText = document.querySelector('.tracker__text');
-let trackerNumber = document.querySelector('.tracker__amount__number');
-let trackerButton = document.querySelector('.tracker__button');
-let trackerData = document.querySelector('.tracker__data');
-let trackerItemText = document.querySelector('.tracker__item__text');
-let trackerItemBalance = document.querySelector('.tracker__item__balance');
-let trackerBalanceNumber = document.querySelector('.tracker__balance__number');
-let trackerIncomeBalance = document.querySelector('.tracker__income__balance');
-let trackerExpenseBalance = document.querySelector('.tracker__expense__balance'); 
+let elementText = document.querySelector('.tracker__text');
+let elementNumber = document.querySelector('.tracker__amount__number');
+let elementButton = document.querySelector('.tracker__button');
+let elementData = document.querySelector('.tracker__data');
+let elementItText = document.querySelector('.tracker__item__text');
+let elementItBalance = document.querySelector('.tracker__item__balance');
+let elementBalance = document.querySelector('.tracker__balance__number');
+let elementInBalance = document.querySelector('.tracker__income__balance');
+let elementExpBalance = document.querySelector('.tracker__expense__balance'); 
 
-trackerButton.onclick = function() {
-    let newTrackerItem = document.createElement('div');
-    let newTrackerItemText = document.createElement('p');
-    newTrackerItemText.className = 'tracker__item__text';
-    let newTrackerItemBalance = document.createElement('p');
-    newTrackerItemBalance.className = 'tracker__item__balance';
-    trackerItemText = trackerText.value;
-    trackerItemBalance = trackerNumber.value;
-    newTrackerItemText.textContent = trackerItemText;
-    newTrackerItemBalance.textContent = trackerItemBalance;
-    newTrackerItem.append(newTrackerItemText, newTrackerItemBalance);
-    trackerBalanceNumber.textContent = `$${trackerItemBalance}.00`; 
+let myCards = [];
 
+const funcMyCards = function(myCard) {
+    console.log(myCard);
+};
 
-    if (trackerItemBalance < 0) {
-        newTrackerItem.classList.add('negative');
-        trackerExpenseBalance.textContent = `$${trackerItemBalance}.00`;
+myCards.forEach(funcMyCards);
+
+elementButton.onclick = function() {
+    let newElemItem = document.createElement('div');
+    newElemItem.className = 'tracker__item';
+    let newElemText = document.createElement('p');
+    newElemText.className = 'tracker__item__text';
+    let newElemBalance = document.createElement('p');
+    newElemBalance.className = 'tracker__item__balance';
+    elementItText = elementText.value;
+    elementItBalance = elementNumber.value;
+    newElemText.textContent = elementItText;
+    newElemBalance.textContent = elementItBalance;
+    newElemItem.append(newElemText, newElemBalance);
+    elementBalance.textContent = `$${elementItBalance}.00`; 
+
+    let newCard = {
+        text: elementItText,
+        number: elementItBalance
     };
 
-    if (trackerItemBalance > 0) {
-        newTrackerItem.classList.add('positive');
-        trackerIncomeBalance.textContent = `$${trackerItemBalance}.00`;
+    myCards.push(newCard);
+
+
+    if (elementItBalance < 0) {
+        newElemItem.classList.add('negative');
+        elementExpBalance.textContent = `$${elementItBalance}.00`;
     };
 
-    trackerData.append(newTrackerItem);
+    if (elementItBalance > 0) {
+        newElemItem.classList.add('positive');
+        elementInBalance.textContent = `$${elementItBalance}.00`;
+    };
 
+    elementData.append(newElemItem);
 }
