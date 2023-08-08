f36a7a40400a11ceadaafd6802175ef58c5a23dc
rezniki <igorreznik2001@gmail.com>
2023-08-08 18:53:33 +0300

    Save data in array and add new card

--- app.js
+++ app.js
@@ -2,34 +2,25 @@ let elementText = document.querySelector('.tracker__text');
 let elementNumber = document.querySelector('.tracker__amount__number');
 let elementButton = document.querySelector('.tracker__button');
 let elementData = document.querySelector('.tracker__data');
-let elementItText = document.querySelector('.tracker__item__text');
-let elementItBalance = document.querySelector('.tracker__item__balance');
 let elementBalance = document.querySelector('.tracker__balance__number');
 let elementInBalance = document.querySelector('.tracker__income__balance');
 let elementExpBalance = document.querySelector('.tracker__expense__balance'); 
 
 let myCards = [];
 
-elementButton.onclick = function() {
-    let newCard = {
-        text: elementItText,
-        balance: +elementItBalance,
-    };
-
-    myCards.push(newCard);
-    console.log(myCards);
-};
-
 let renderData = function() {
     const funcData = function(myCard) {
         let newElemItem = document.createElement('div');
-        newElemItem.className = 'tracker__item';
         let newElemText = document.createElement('p');
-        newElemText.className = 'tracker__item__text';
         let newElemBalance = document.createElement('p');
+
+        newElemItem.className = 'tracker__item';
+        newElemText.className = 'tracker__item__text';
         newElemBalance.className = 'tracker__item__balance';
-        newElemText.textContent = myCard.value;
-        newElemBalance.textContent = myCard.value;
+
+        newElemText.textContent = myCard.text;
+        newElemBalance.textContent = myCard.balance;
+        
         newElemItem.append(newElemText, newElemBalance);
         elementData.append(newElemItem);
     }
@@ -38,4 +29,21 @@ let renderData = function() {
 };
 
 renderData();
+
+elementButton.onclick = function() {
+    let elemItText = elementText.value;
+    let elemItBalance = elementNumber.value;
+
+    let newCard = {
+        text: elemItText,
+        balance: +elemItBalance,
+    };
+
+    myCards.push(newCard);
+    elementData.innerHTML = "";
+    renderData();
+    console.log(myCards);
+};
+
+
 
